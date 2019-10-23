3fcd707a066f5218a1c884510e244218147f420a
Seljuke <seljukspi@gmail.com>
2019-10-23 15:52:11 +0300

    falke8 warnings fixed

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -14,8 +14,10 @@ def test_hosts_file(host):
     assert f.user == 'root'
     assert f.group == 'root'
 
+
 def test_sshd_inactive(host):
     assert host.service("zookeeper").is_running
+
 
 def test_zookeeper_port(host):
     assert host.socket("tcp://0.0.0.0:2181").is_listening
